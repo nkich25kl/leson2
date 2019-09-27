eaa329f8ac6ea9b9a945b2adfe6f443e9f1cf403
mobile3 <mobile3@itcube.win>
2019-09-27 18:03:41 +0300

    типы операций

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	float f1 = 123456.1234567F;
+	float f2  = 1000.123456789F;
+        System.out.format("%.10f%n",f1);
+        System.out.format("%.10f%n",f2);
+    }
+}
